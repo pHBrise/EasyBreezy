bc41beddcbee91f4a6b51f5bf09095cd9113d151
Breeze <phbrise@gmail.com>
2025-03-23 14:27:40 +0700

    Initial Commit

---
+++ EasyBreezy/EasyBreezyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyBreezyApp.swift
+//  EasyBreezy
+//
+//  Created by Phanvit Chevamongkolnimit on 23/3/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct EasyBreezyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EasyBreezyTests/EasyBreezyTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyBreezyTests.swift
+//  EasyBreezyTests
+//
+//  Created by Phanvit Chevamongkolnimit on 23/3/2568 BE.
+//
+
+import Testing
+@testable import EasyBreezy
+
+struct EasyBreezyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
